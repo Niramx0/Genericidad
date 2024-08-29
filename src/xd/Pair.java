afc1bf71a6952c2b8b4c9b7a2f1baa31422c03eb
Your Name <you@example.com>
2024-08-29 10:29:09 -0500

    Commit Final / programa terminado

---
+++ src/xd/Pair.java
@@ -0,0 +1,26 @@
+package xd;
+
+public class Pair <T extends Number, U>{
+    protected T first;
+    protected U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public void devuelveDatos(T t,U u){
+        
+    }
+    
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+    
+    
+    
+}
---
+++ src/xd/XD.java
@@ -0,0 +1,68 @@
+package xd;
+
+/*
+Ejercicio:
+
+
+Crear una clase genérica llamada Pair<T,
+U> que represente un par de elementos de tipos
+diferentes T y U.
+
+
+·       Atributos:
+
+
+·       T first
+
+
+·       U second
+
+
+Constructor:
+
+
+·      
+Un constructor que recibe dos argumentos de
+tipo T y U para inicializar los atributos.
+
+
+Métodos:
+
+
+·      
+getFirst(): Devuelve el primer elemento del par.
+
+
+·      
+getSecond(): Devuelve el segundo elemento del par.
+
+
+·      
+toString(): Devuelve una representación en cadena del
+par en formato "(first, second)".
+
+
+La idea es utilizar las clase genérica para almacenar
+los datos de edad y nombre de una persona.
+
+
+Posteriormente, con la misma clase se solicitan los
+datos de eps y fecha de nacimiento.
+
+
+Estos dos objetos alimentaran una base de datos de
+usuarios
+*/
+/**
+ *
+ * @author OMAR ALEJANDRO SALAZAR CARVAJAL
+ */
+public class XD {
+
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
